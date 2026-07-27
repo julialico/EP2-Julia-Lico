9dfc2563047da1cdc795116ea86b7fa980e3d7c2
Julia Lico <juliamal@al.insper.edu.br>
2026-07-27 19:02:02 -0300

    algoritmo da jogada

--- jogo.py
+++ jogo.py
@@ -31,3 +31,80 @@ def baseconsistente(lista):
             tela.escreveerro('É preciso ter pelo menos 3 perguntas de nivel "' + nivel + '" na base!')
  
     return not temerro
+
+def leentrada(mensagem):
+    entrada = input(mensagem)
+    entrada = entrada.strip().upper()
+    return entrada
+ 
+ 
+def jogapartida(basepornivel):
+    nome = input('Qual é o seu nome? ').strip()
+    if nome == '':
+        nome = 'Jogador'
+ 
+    tela.exibemanual()
+ 
+    premioatual = 0
+    pulosrestantes = PULOSINICIAIS
+    ajudasrestantes = AJUDASINICIAIS
+    sorteadas = []
+    indice = 0
+ 
+    while indice < len(PREMIOS):
+        nivel = FAIXAS[indice]
+        questao = sorteia_questao_inedita(basepornivel, nivel, sorteadas)
+        ajudausada = False
+        continuapergunta = True
+ 
+        while continuapergunta:
+            tela.exibeestado(nome, premioatual, pulosrestantes, ajudasrestantes)
+            print(questao_para_texto(questao, indice + 1))
+ 
+            entrada = leentrada('Escolha A, B, C, D, PULA ou AJUDA: ')
+ 
+            if entrada not in OPCOESVALIDAS:
+                tela.escreveerro('Opcao invalida! Digite A, B, C, D, PULA ou AJUDA.')
+ 
+            elif entrada == 'PULA':
+                if pulosrestantes > 0:
+                    pulosrestantes = pulosrestantes - 1
+                    questao = sorteia_questao_inedita(basepornivel, nivel, sorteadas)
+                    ajudausada = False
+                    tela.escreveAviso('Pergunta trocada! Pulos restantes: ' + str(pulosrestantes))
+                else:
+                    tela.escreveErro('Voce nao tem mais pulos disponiveis!')
+ 
+            elif entrada == 'AJUDA':
+                if ajudausada:
+                    tela.escreveerro('Voce ja usou ajuda nesta pergunta, nao pode usar de novo!')
+                elif ajudasrestantes <= 0:
+                    tela.escreveerro('Voce nao tem mais ajudas disponiveis!')
+                else:
+                    ajudasrestantes -= 1
+                    ajudausada = True
+                    tela.escreveinfo(gera_ajuda(questao))
+ 
+            elif entrada == questao['correta']:
+                premioatual = PREMIOS[indice]
+                tela.escrevesucesso('Resposta correta!')
+                tela.escrevepremio(premioatual)
+ 
+                if premioatual == PREMIOS[-1]:
+                    tela.escrevesucesso('PARABENS ' + nome + '! Voce ganhou o premio maximo!')
+                    return premioatual
+ 
+                quer = leentrada('Deseja PARAR e sair com o premio, ou CONTINUAR jogando? (PARAR/CONTINUAR): ')
+                if quer == 'PARAR':
+                    tela.escrevesucesso(nome + ', voce saiu do jogo com R$ ' + tela.formatamoeda(premioatual) + '!')
+                    return premioatual
+                else:
+                    indice = indice + 1
+                    continuapergunta = False
+ 
+            else:
+                tela.escreveerro('Resposta errada! A alternativa correta era: ' + questao['correta'])
+                tela.escreveerro(nome + ', voce perdeu tudo e sai sem nenhum premio!')
+                return 0
+ 
+    return premioatual
